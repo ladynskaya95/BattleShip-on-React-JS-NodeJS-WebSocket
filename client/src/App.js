0c7a5f29f1a6f646a728d76dc3ccdfa9f3b1737f
Nastya Ladynskaya <feverrr27@gmail.com>
2023-05-12 19:27:07 +0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css';
+import Login from "./pages/Login";
+import GamePage from "./pages/GamePage";
 
 function App() {
   return (
     <div className="App">
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/game">
+          <Route path=":gameId" element={<GamePage/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
