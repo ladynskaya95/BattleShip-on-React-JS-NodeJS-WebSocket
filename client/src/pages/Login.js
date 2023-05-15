fdf195d587ac883aeda3d000c5e32e90715859f0
Nastya Ladynskaya <feverrr27@gmail.com>
2023-05-15 14:53:27 +0300

    authorization form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -17,8 +17,74 @@ const Login = () => {
   }
 
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h2>Авторизація</h2>
+      <form onSubmit={startPlay}>
+        <div className="field-group">
+          <div>
+            <label htmlFor="nickname">Ваше ім'я</label>
+          </div>
+          <input
+            type="text"
+            name="typeEnter"
+            id="nickname"
+            onChange={(e) => setNickname(e.target.value)}
+          />
+          </div>
+          <div
+            onChange={(e) => setInvintationGame(e.target.id === "ingame")}
+            className="field-group"
+          >
+            <input
+              type="radio"
+              name="typeEnter"
+              id="gen"
+              value={!invintationGame}
+              defaultChecked={!invintationGame}
+            />
+            <label htmlFor="gen">Створити гру</label>
+            <input
+              type="radio"
+              name="typeEnter"
+              id="ingame"
+              value={!invintationGame}
+              defaultChecked={!invintationGame}
+            />
+            <label htmlFor="ingame">Увійти у гру за ідентифікатором</label>
+          </div>
+          <div className="field-group">
+            {invintationGame ? (
+              <>
+              <div>
+                <label htmlFor="gameId">Введіть індентифікатор гри</label>
+              </div>
+                <input
+                  type="text"
+                  name="gameId"
+                  defaultValue=""
+                  id="nickname"
+                  onChange={(e) => setGameId(e.target.value)}
+          />
+              </>
+            ) : (
+              <>
+                <button 
+                  className='btn-gen'
+                  onClick={(e) => {
+                      e.preventDefault()
+                      setGameId(Date.now())
+                  }}
+                >
+                  Згенерувати ідентифікатор гри
+                </button>
+                <p>{gameId}</p>
+              </>
+            )}
+        </div>
+        <button type="submit" className='btn-ready'>ГРАТИ</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login
