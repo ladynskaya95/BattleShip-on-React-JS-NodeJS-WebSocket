41e39cb3eb24a4c7b7a12f4c831aafbe7a8dec6c
Nastya Ladynskaya <feverrr27@gmail.com>
2023-05-16 14:32:08 +0300

    create methods for board models

--- client/src/models/Board.js
+++ client/src/models/Board.js
@@ -1,15 +1,24 @@
 import {Cell } from "./Cell"
 
 export class Board {
-    cells = [];
+  cells = [];
 
-    initCells() {
-        for (let i = 0; i < 10; i++) {
-            const row = [];
-            for (let j = 0; j < 10; j++) {
-                row.push(new Cell(this, j, i, null));
-                }
-            this.cells.push(row)    
-        }
+  initCells() {
+    for (let i = 0; i < 10; i++) {
+      const row = [];
+      for (let j = 0; j < 10; j++) {
+        row.push(new Cell(this, j, i, null));
+      }
+      this.cells.push(row);
     }
+  }
+  getCopyBoard() {
+    const newBoard = new Board();
+    newBoard.cells = this.cells;
+    return newBoard;
+  }
+
+  getCells(x, y) {
+    return this.cells[y][x];
+  }
 }
